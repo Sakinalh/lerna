422ff7b2f00867c7055371e46dc8b1de7cd974c1
Sakina D <sa.debiche@gmail.com>
2022-02-28 10:48:09 +0100

    changer integration folder content

--- packages/integration/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { doctor } from "doctor";
-import { patient } from "patient";
-import { scheduler } from "scheduler";
-
-console.log(new doctor());
-console.log(new scheduler());
-console.log(new patient());
